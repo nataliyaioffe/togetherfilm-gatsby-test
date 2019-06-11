af29f61f2d2b35ee3a400d9d73a4729182dfe041
Nataliya Ioffe <nioffe@habaneroconsulting.com>
2019-06-11 13:33:16 -0400

    Accessing body of blog from contentful, and drilling into json objects to access published image.

--- src/templates/blog.js
+++ src/templates/blog.js
@@ -1,6 +1,7 @@
 import React from "react"
 import Layout from "../components/layout"
 import { graphql } from "gatsby"
+import { documentToReactComponents } from "@contentful/rich-text-react-renderer"
 
 // For template files like this one, cant use usestaticquery.
 // We must define query separately and export it
@@ -12,15 +13,32 @@ export const query = graphql`
     contentfulBlogPost(slug: { eq: $slug }) {
       title
       publishedDate(formatString: "MMMM Do, YYYY")
+      body {
+        json
+      }
     }
   }
 `
 
 const Blog = props => {
+  const options = {
+    renderNode: {
+      "embedded-asset-block": node => {
+        const alt = node.data.target.fields.title["en-US"]
+        const url = node.data.target.fields.file["en-US"].url
+        return <img alt={alt} src={url} />
+      },
+    },
+  }
+
   return (
     <Layout>
       <h1>{props.data.contentfulBlogPost.title}</h1>
       <p>{props.data.contentfulBlogPost.publishedDate}</p>
+      {documentToReactComponents(
+        props.data.contentfulBlogPost.body.json,
+        options
+      )}
     </Layout>
   )
 }
